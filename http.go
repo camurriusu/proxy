bbecc96c7ae92b43f81d712cc199040ea68c856f
Riccardo Riggi <riggir@tcd.ie>
2026-03-01 20:25:10 +0000

    Add option to block HTTP by hostname as well as full URL

--- http.go
+++ http.go
@@ -15,11 +15,15 @@ import (
 func handleHTTP(request *http.Request, clientConn net.Conn) error {
 	start := time.Now()
 
-	// Check block list for full URL
+	// Check block list for either hostname or full URL
+	hostName := request.URL.Hostname()
+	isBlocked, found := blocklist.Get(hostName)
+
 	url := request.URL.String()
-	isBlocked, found := blocklist.Get(url)
+	if !found || isBlocked.(bool) == false {
+		isBlocked, found = blocklist.Get(url)
+	}
 	if found && isBlocked.(bool) == true {
-		fmt.Println("Blocked request to", url)
 		response := "HTTP/1.1 403 Forbidden\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<html><body><h1>403 Forbidden: URL Blocked by Proxy</h1></body></html>\n"
 		clientConn.Write([]byte(response))
 		return errors.New("Request to " + url + " blocked by proxy")
@@ -35,7 +39,7 @@ func handleHTTP(request *http.Request, clientConn net.Conn) error {
 	key := request.URL.String()
 	// Check cache only if request is GET
 	if request.Method == http.MethodGet {
-		data, found := cache.Get(key)
+		data, found := cache.Get(url)
 		if found {
 			clientConn.Write(data.([]byte))
 			fmt.Printf("HIT %s in %v\n", key, time.Since(start))
